a5f0f6b6f0d5c54174e65fd19667e7149e25622c
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-11-22 15:30:18 -0300

    Update: Ajuste na rota home

--- app_odonto/routes/web.php
+++ app_odonto/routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
     return redirect()->route('admin.inicio');
 });
 
-Route::get('/login', [LoginController::class, 'inicio'])->name('admin.inicio');
+Route::get('/home', [LoginController::class, 'inicio'])->name('admin.inicio');
 Route::post('/login', [LoginController::class, 'login'])->name('admin.login');
 Route::get('/logout', [LoginController::class, 'logout'])->name('admin.logout');
 Route::get('/login/auth/google', [LoginController::class, 'redirectToGoogle'])->name('admin.login_google');
